1f674540ffc87ddb5df1a053dac35fa71e9317a5
chpark <whitenight209@gmail.com>
2024-07-04 15:52:55 +0900

    add log

--- src/main/java/com/nexr/azure/azuredemo/infrastructure/MyController.java
+++ src/main/java/com/nexr/azure/azuredemo/infrastructure/MyController.java
@@ -1,17 +1,20 @@
 package com.nexr.azure.azuredemo.infrastructure;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class MyController {
 
     @GetMapping("/hello")
     public String helloWorld() {
+        log.info("request time {}", LocalDateTime.now());
         return "Hello World! " + LocalDateTime.now();
     }
 }
