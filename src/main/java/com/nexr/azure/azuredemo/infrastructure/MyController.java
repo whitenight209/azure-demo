35e6005d450a425065658872aabb3d182c440c6f
chpark <whitenight209@gmail.com>
2024-07-12 12:23:55 +0900

    add error request mapping

--- src/main/java/com/nexr/azure/azuredemo/infrastructure/MyController.java
+++ src/main/java/com/nexr/azure/azuredemo/infrastructure/MyController.java
@@ -1,6 +1,7 @@
 package com.nexr.azure.azuredemo.infrastructure;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,11 @@ public class MyController {
     public String helloWorld() {
         log.info("request time {}", LocalDateTime.now());
         return "Hello World! " + LocalDateTime.now();
+    }
+
+    @GetMapping("/internal-error")
+    public ResponseEntity<String> randomError() {
+        log.error("internal error in {}", LocalDateTime.now());
+        return ResponseEntity.internalServerError().body("Internal Server Error");
     }
 }
